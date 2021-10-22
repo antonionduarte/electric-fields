4598fe5372fcf784ff6cfa98f078bb4bb31ba739
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2021-10-22 15:33:57 +0100

    Comments

--- src/app.js
+++ src/app.js
@@ -82,7 +82,7 @@ function setup(shaders) {
 		if (event.code == 'Space') {
 			cVisible = !cVisible;
 		}
-	})
+	});
 	line_slider.oninput = () => {
 		lineLength = line_slider.value;
 	}
@@ -164,16 +164,16 @@ function resizeCanvas() {
 
 /**
  * Function responsible for drawing the points or lines between two vertices.
- * @param {Array} uniforms
- * @param {} buffer
- * @param {} attribute
- * @param {int} amount
- * @param {int} vecSize
- * @param {} glMode
- * @param {int} stride
- * @param {int} offset
+ * @param {Array} uniforms array of uniforms and corresponding values (only simple uniform1f).
+ * @param {WebGLBuffer} buffer the buffer to use.
+ * @param {number} attribute the attribute to use.
+ * @param {int} amount the amount of objects to draw.
+ * @param {int} elemSize the size in bytes of each element to draw.
+ * @param {number} glMode the glMode to draw with (could be gl.LINES, gl.POINTS etc...)
+ * @param {int} stride the stride inside the buffer between each element.
+ * @param {int} offset the offset inside the buffer before the start of the element.
 */
-function drawPoints(uniforms, buffer, attribute, amount, vecSize, glMode, stride, offset) {    
+function drawPoints(uniforms, buffer, attribute, amount, elemSize, glMode, stride, offset) {    
 	for (let i in uniforms) {
 		let location = (uniforms[i])[0];
 		let value = (uniforms[i])[1];
@@ -182,7 +182,7 @@ function drawPoints(uniforms, buffer, attribute, amount, vecSize, glMode, stride
 
 	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 	gl.enableVertexAttribArray(attribute);
-	gl.vertexAttribPointer(attribute, vecSize, gl.FLOAT, false, stride, offset);
+	gl.vertexAttribPointer(attribute, elemSize, gl.FLOAT, false, stride, offset);
 	gl.drawArrays(glMode, 0, amount)
 	gl.disableVertexAttribArray(attribute);
 }
